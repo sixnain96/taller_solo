db94c79df7b47bddfecfdcb0b8d8217d4267b806
Juan Jose <juanmtg985@gmail.com>
2026-04-21 20:28:26 -0500

    chore: initialize bookstore api project

---
+++ src/main/java/com/taller/bookstore/service/AuthService.java
@@ -0,0 +1,10 @@
+package com.taller.bookstore.service;
+
+import com.taller.bookstore.dto.request.LoginRequest;
+import com.taller.bookstore.dto.request.RegisterRequest;
+import com.taller.bookstore.dto.response.AuthResponse;
+
+public interface AuthService {
+    AuthResponse register(RegisterRequest request);
+    AuthResponse login(LoginRequest request);
+}
